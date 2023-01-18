d28cd563c349a3bc89f00b91752f4fa525b40a02
Sagnik Chatterjee <sagnikchatterjee395@gmail.com>
2023-01-18 16:56:44 +0530

    Src : solved part1

---
+++ aoc2019/day1/part1.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+const main = () => {
+  try {
+    const data = fs.readFileSync("./input.txt", "utf-8");
+    const arr = data.split("\n").map((item) => parseInt(item)).filter(Boolean);
+    let fuelRequired = 0;
+    arr.forEach((item) => {
+      item = Math.round(parseFloat(item) / 3);
+      item = item - 2;
+      fuelRequired += item;
+    });
+    console.log(fuelRequired);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+main();
