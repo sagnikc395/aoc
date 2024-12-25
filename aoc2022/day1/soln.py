b8602c4fb01790fbf641ba73cf3411b3d0a7bdfb
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-12-25 11:41:28 +0530

    src: solved part1

---
+++ aoc2022/day1/soln.py
@@ -0,0 +1,40 @@
+def part1():
+    with open('./input.txt','r') as f:
+        data = f.read().strip().split('\n')
+
+    elf_calorie_counter = {}
+    elf_counter = 0
+
+    sum = 0
+    for item in data:
+        if item != '':
+            sum += int(item)
+            elf_calorie_counter[elf_counter] = sum
+        else:
+            elf_counter += 1
+            sum = 0
+
+    highest_elf = max(elf_calorie_counter.items(), key=lambda item: item[1])
+    print(highest_elf)
+
+def part2():
+    pass
+
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(description="AoC 2022, Day1")
+    parser.add_argument("--part1",action="store_true",help="Solve Part1")
+    parser.add_argument("--part2",action="store_true",help="Solve Part2")
+
+    args = parser.parse_args()
+
+    if args.part1:
+        part1()
+    elif args.part2:
+        part2()
+
+if __name__ == '__main__':
+    main()
