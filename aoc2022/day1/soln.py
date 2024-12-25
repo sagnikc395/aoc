487b4cf96f21d2da32a9065936bf81aed2dc9999
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-12-25 11:47:26 +0530

    src: solved part2

--- aoc2022/day1/soln.py
+++ aoc2022/day1/soln.py
@@ -18,8 +18,27 @@ def part1():
     print(highest_elf)
 
 def part2():
-    pass
+    with open('./input.txt','r') as f:
+        data = f.read().strip().split('\n')
 
+    elf_calorie_counter = {}
+    elf_counter = 0
+
+    sum = 0
+    for item in data:
+        if item != '':
+            sum += int(item)
+            elf_calorie_counter[elf_counter] = sum
+        else:
+            elf_counter += 1
+            sum = 0
+
+    top_three_highest_elf =  sorted(elf_calorie_counter.items(),key=lambda item: item[1],reverse=True)[:3]
+
+    result = 0
+    for _, v in top_three_highest_elf:
+        result += v
+    print(result)
 
 
 def main():
